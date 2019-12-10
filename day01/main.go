4e642ff75e2103e020cb02e3b8bdfb02e20df266
Michael Weidmann <michaelweidmann@web.de>
2019-12-10 16:06:26 +0100

    initial commit and implementation of the first day

---
+++ day01/main.go
@@ -0,0 +1,58 @@
+package day01
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Run(part string) {
+	if part == "1" {
+		partOne()
+	} else {
+		partTwo()
+	}
+}
+
+func partOne() {
+	file, _ := os.Open("day01/input.txt")
+	defer file.Close()
+
+	var sum int = 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		mass, _ := strconv.Atoi(scanner.Text())
+		sum += calcFuel(&mass)
+	}
+
+	fmt.Printf("Solution: %d\n", sum)
+}
+
+func partTwo() {
+	file, _ := os.Open("day01/input.txt")
+	defer file.Close()
+
+	var sum int = 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		mass, _ := strconv.Atoi(scanner.Text())
+
+		for calcFuel(&mass) > 0 {
+			sum += mass
+		}
+	}
+
+	fmt.Printf("Solution: %d", sum)
+}
+
+/*---------------------------------------------------------------------*/
+/*                         Util Function(s)	                           */
+/*---------------------------------------------------------------------*/
+
+func calcFuel(mass *int) int {
+	*mass = (*mass / 3) - 2
+	return *mass
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/michaelweidmann/advent-of-code-2019/day01"
+)
+
+func main() {
+	arguments := os.Args
+
+	if len(arguments) != 3 {
+		printUsage(arguments[0])
+		os.Exit(1)
+	}
+
+	dayNumber, _ := strconv.Atoi(arguments[1])
+
+	switch dayNumber {
+	case 1:
+		day01.Run(arguments[2])
+		break
+	default:
+		printUsage(arguments[0])
+		os.Exit(1)
+	}
+}
+
+func printUsage(programName string) {
+	fmt.Printf("Usage: %s [<day-number>] [<part-number>]\n\n", programName)
+	fmt.Println("Allowed <day-number> values are: 1 to 24")
+	fmt.Println("Allowed <part-number> values are: 1 or 2")
+}
